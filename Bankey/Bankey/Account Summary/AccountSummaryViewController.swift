d8125e748e3f9dcbb1182799ae24f7a56ed4577d
ryglis <85841779+ryglis@users.noreply.github.com>
2023-09-24 11:22:22 +0200

    feat: Handle network errors

--- Bankey/Bankey/Account Summary/AccountSummaryViewController.swift
+++ Bankey/Bankey/Account Summary/AccountSummaryViewController.swift
@@ -142,7 +142,7 @@ extension AccountSummaryViewController {
 //                self.configureTableHeaderView(with: profile)
 //                self.tableView.reloadData() //wywalamy stąd żeby grupowo to działało
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -154,7 +154,7 @@ extension AccountSummaryViewController {
                 self.accounts = accounts
 
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -185,6 +185,31 @@ extension AccountSummaryViewController {
                                          accountName: $0.name,
                                          balance: $0.amount)
         }
+    }
+    
+    private func displayError(_ error: NetworkError) {
+        let title: String
+        let message: String
+        switch error {
+        case .serverError:
+            title = "Server Error"
+            message = "We could not process your request. Please try again."
+        case .decodingError:
+            title = "Network Error"
+            message = "Ensure you are connected to the internet. Please try again."
+        }
+        self.showErrorAlert(title: title, message: message)
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
